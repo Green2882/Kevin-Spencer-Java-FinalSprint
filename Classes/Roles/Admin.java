ee89989eb94ba7004cff59992f6363c023a797cf
Kevin Green <Greenkevin28@icloud.com>
2025-12-08 14:03:53 -0330

    Created Admin.java and Trainer.java

--- Classes/Roles/Admin.java
+++ Classes/Roles/Admin.java
@@ -1,5 +1,18 @@
 package Roles;
 
-public class Admin {
+public class Admin extends Users.User {
 
+    // Constructor
+    public Admin(String userName, String password, String email, String phone, String role, int userId) {
+        super(userName, password, email, phone, role, userId);
+    }
+
+    public Admin() {
+    }
+
+    // toString method  
+    @Override
+    public String toString() {
+        return "Admin{" + super.toString() + '}';
+    }
 }
