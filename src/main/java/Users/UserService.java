a5100c1754347aecec4893b34ee67033b80a24dc
Kevin Green <Greenkevin28@icloud.com>
2025-12-09 12:21:46 -0330

    Readded pom.xml file as it seems to have dissapeared

--- src/main/java/Users/UserService.java
+++ src/main/java/Users/UserService.java
@@ -1,5 +1,13 @@
 package Users;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class UserService {
-    
+
+    UserDAO userDAO = new UserDAO();
+
+    public void saveNewUser(User user) {
+
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(8));
+    }
 }
